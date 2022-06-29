a31927b5c85f03f144103255553a70512293510e
kibalias <74294078+kibalias@users.noreply.github.com>
2022-06-29 23:30:02 +0800

    Created header.php and footer.php; moved files to new directories

---
+++ tailwind/css/public/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="styles.css">
+    <title></title>
+  </head>
+  <body class="text-green-900">
+    <div class="">
+      <div>
+      <!--HEADER-->
+      <?php include 'header.php';?>
+      </div>
+      <div class="w-full h-96 relative border-2 group">
+        <img src="images/vet.png" class="w-full h-96 object-contain absolute mix-blend-overlay bg-blue-400 group-hover:blur">
+        <div class="p-5">
+            <h1 class="text-black text-2xl sm:text-6xl font-body">VetCares</h1>
+            <h2 class="text-black text-sm sm:text-3xl font-light mt-5"> Cares gently for your pets</h2>
+            <div class="">
+
+            </div>
+            <p class="flex items-baseline text-sm text-gray-900 bg-gray-500 sm:font-mono  text-justify mt-10 sm:mt-0 sm:text-2xl justify-end h-full w-full sm:text-black invisible group-hover:visible">VetCares,vows to treat your pets with care and ensure<br> proper treatment regardless of breed or type of animal <br>that will  be brought in our care. We ought to bring the <br> best services to you</p>
+        </div>
+      </div>
+
+      <div class="w-full h-96 relative border-2 group">
+        <img src="images/law.png" class="h-full w-full object-cover absolute mix-blend-overlay bg-blue-400 group-hover:blur">
+        <div class="p-5">
+            <h1 class="text-gray-200 text-2xl sm:text-6xl font-body">VetCares</h1>
+            <h2 class="text-white text-sm sm:text-2xl font-light mt-5">Appointment Policy</h2>
+            <div class="">
+
+            </div>
+            <p class="flex items-baseline text-sm text-gray-300 bg-gray-500 sm:font-mono  text-justify mt-10 sm:mt-0 sm:text-2xl justify-end h-full w-full sm:text-gray-200 invisible group-hover:visible">
+              Same day appointments will be allowed in times of free slots,<br>each working day a total of 6 appointments will be available<br> for VetCares' appointees to take.Appointments shall not be<br> cancelled without prior notice to avoid inconvenience</p>
+        </div>
+      </div>
+
+      <div class="w-full h-96 relative border-2 group">
+        <img src="images/pay.jpg" class="h-full w-full object-cover absolute mix-blend-overlay bg-blue-400 group-hover:blur">
+        <div class="p-5">
+            <h1 class="text-black text-2xl sm:text-6xl font-body">VetCares</h1>
+            <h2 class="text-black text-sm sm:text-2xl font-light mt-5">Appointment Policy</h2>
+            <div class="">
+
+            </div>
+            <p class="flex items-baseline text-sm text-white bg-gray-500 sm:font-mono  text-justify mt-10 sm:mt-0 sm:text-2xl justify-end h-full w-full sm:text-white invisible group-hover:visible">
+              Payment shall be handed over before or after the services has been done,<br>the clinic will accept cash or credit card as payments for customer convenience</p>
+        </div>
+      </div>
+    <!--FOOTER-->
+     <?php include 'footer.php';?>
+    </div>
+
+    <!--JAVASCRIPT FILES-->
+    <script src="javascript/script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  </body>
+</html>
