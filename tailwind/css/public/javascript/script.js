e6a00b4b5c8c9eac0901bd086b920334b72375ae
kibalias <74294078+kibalias@users.noreply.github.com>
2022-07-03 21:44:19 +0800

    Fixed responsive nav bar

--- tailwind/css/public/javascript/script.js
+++ tailwind/css/public/javascript/script.js
@@ -1,7 +1,10 @@
-function expandMenu(e){
-    let list = document.querySelector('ul');
-
-    e.name === 'menu-outline' ? (e.name = "close-outline", list.classList.add('top-[80px]'), list.classList.add('opacity-100'),list.classList.remove('fixed'))
-                                : (e.name = "menu-outline", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'), list.classList.add('fixed'))
+function expandMenu(){
+    /*
+     * Expand or shrink navigation bar links
+     */
+    const btn = document.querySelector('.mobileMenuBtn');
+    const menu = document.querySelector('.mobile-menu');
+    
+    btn.name === 'close-outline' ? (btn.name = "menu-outline", menu.classList.add('hidden')) : (btn.name="close-outline", menu.classList.remove('hidden'))
 
 }
