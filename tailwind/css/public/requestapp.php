35aac50253672475f317dd16950ff819c40ca195
kibalias <74294078+kibalias@users.noreply.github.com>
2022-07-18 11:54:08 +0800

    log out and some modifications

--- tailwind/css/public/requestapp.php
+++ tailwind/css/public/requestapp.php
@@ -9,39 +9,45 @@
   </head>
   <body>
     <?php include 'clientsidebar.html' ?>
-        <h1 class="m-auto text-4xl font-extrabold justify-center mt-5 text-center">Records</h1>
+    <h1 class="md:ml-16 m-auto text-4xl text-white font-bold justify-center mt-10 text-center">Set Appointment</h1>
         <div class="">
           <div class=" ml-20 mt-20 md:mt-20 md:ml-60 md:mr-4 w-full h-60">
-            <form class="" action="index.html" method="post">
-              <label for="date" class="font-bold w-2">Date</label>
-              <input type="date" name="date" placeholder="Appointment Date" class="ml-9 bg-blue-200 md:bg-blue-300 border-b-2 border-black  ">
+            <form class="" action="#" method="post">
+              <div class="my-2">
+                <label for="date" class="font-bold w-2">Date</label>
+                <input type="date" name="date" placeholder="Appointment Date" class="ml-9 bg-blue-200 md:bg-blue-300 border-b-2 border-black  ">
+              </div>
 
-              <br><label for="time" class="font-bold">Time</label>
-              <input type="text" name="time" placeholder="Appointment Time" class="ml-9 bg-blue-200 md:bg-blue-300 border-b-2 border-black"><br>
+              <div class="my-2">
+                <label for="time" class="font-bold">Time</label>
+                <input type="text" name="time" placeholder="Appointment Time" class="ml-9 bg-blue-200 md:bg-blue-300 border-b-2 border-black">
+              </div>
 
-              <label for="time" class="font-bold w-1/12">Time</label>
-              <input type="text" name="time" placeholder="Time" class="ml-9 bg-blue-200 md:bg-blue-300 border-b-2 border-black  ">
+              <div class="my-2">
+                <label for="pet" class="font-bold w-1/12">Pet Name</label>
+                <select class=" bg-blue-200 md:bg-blue-300 border-b-2 border-black" name="pet">
+                  <option selected>Select a Pet Type</option>
+                  <option value="">Pet 1</option>
+                  <option value="">Pet 2</option>
+                  <option value="">Pet 3</option> <!--Temporary placeholder; might just retrieve from database-->
+                </select>
+              </div>
 
-              <br><label for="pet" class="font-bold w-1/12">Pet Type</label>
-              <input type="text" name="pet" placeholder="Pet Type" class="ml-2 bg-blue-200 md:bg-blue-300 border-b-2 border-black  ">
+              <div class="my-2">
+                <label for="service" class="font-bold w-1/12">Service(s)</label>
+                <select class=" bg-blue-200 md:bg-blue-300 border-b-2 border-black" name="service">
+                  <option value="">Examinations and Diagnosis</option>
+                  <option value="">Endoscopy</option>
+                  <option value="">Laboratory</option>
+                  <option value="">Pharmacy</option>
+                  <option value="">Health Certificates</option>
+                </select>
+              </div>
 
-              <br><label for="breed" class="font-bold w-1/12">Breed</label>
-              <input type="text" name="breed" placeholder="Pet Breed" class="ml-7 bg-blue-200 md:bg-blue-300 border-b-2 border-black  ">
-
-              <br><label for="age" class="font-bold w-1/12">Vet</label>
-              <input type="text" name="age" placeholder="Pet's Age" class="ml-12 bg-blue-200 md:bg-blue-300 border-b-2 border-black  ">
-
-              <br><label for="service" class="font-bold w-1/12">Service(s)</label>
-              <select class=" bg-blue-200 md:bg-blue-300 border-b-2 border-black" name="service">
-                <option value="">Examinations and Diagnosis</option>
-                <option value="">Endoscopy</option>
-                <option value="">Laboratory</option>
-                <option value="">Pharmacy</option>
-                <option value="">Health Certificates</option>
-              </select>
-
-              <br>
-              <button type="submit" name="button" class="ml-20 mt-5 border-black border-2 p-2 rounded-md">Request Appointment</button>
+              <input 
+                    type="submit" 
+                    value="Request Appointment" 
+                    class="my-2 border-gray-600 px-3 p-2 rounded bg-white text-black shadow-md hover:bg-blue-800 hover:text-white hover:shadow-lg">
             </form>
           </div>
         </div>
