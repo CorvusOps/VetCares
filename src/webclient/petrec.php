c94094e6e7d7d908baad6c4747606f2366d9954c
kibalias <74294078+kibalias@users.noreply.github.com>
2022-07-24 23:29:53 +0800

    Public Side and Client Migration

---
+++ src/webclient/petrec.php
@@ -0,0 +1,58 @@
+<?php
+include '../includes/connectdb.php';
+
+
+	if($_SESSION['client_sid']==session_id())
+	{
+		?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/styles.css">
+    <meta charset="utf-8">
+    <title>Pet Information</title>
+  </head>
+  <body class="w-full h-full bg-blue-200 md:bg-blue-300">
+
+  <?php include 'clientsidebar.html' ?>
+
+  <h1 class="md:ml-16 m-auto text-4xl text-white font-bold justify-center mt-10 text-center">Pets' Information</h1>
+
+      <table class="border-2 border-blue-800 m-auto md:mt-20 md:ml-56 md:mr-4 w-9/12 text-left border-collapse lg:ml-60">
+
+        <tr class="border-2 border-blue-800">
+          <th class="w-1/5 border-2 border-blue-800">Pet's Name</th>
+          <th class="w-1/5 border-2 border-blue-800">Owner</th>
+          <th class="w-1/5 border-2 border-blue-800">Age</th>
+          <th class="w-1/5 border-2 border-blue-800">Pet Type</th>
+          <th class="w-1/5 border-2 border-blue-800">Breed</th>
+          <th class="w-1/5 border-2 border-blue-800">Veterinary</th>
+        </tr>
+        <tr>
+          <td class="border-2 border-blue-800">Doggy</td>
+          <td class="border-2 border-blue-800">Own</td>
+          <td class="border-2 border-blue-800">2 years old</td>
+          <td class="border-2 border-blue-800">Doge</td>
+          <td class="border-2 border-blue-800">German Shepherd</td>
+          <td class="border-2 border-blue-800">Dr. Doge</td>
+        </tr>
+      </table>
+
+  </body>
+</html>
+<?php
+    }else
+	{
+		if($_SESSION['admin_sid']==session_id()){
+			header("location:404.php");		
+		}
+		else{
+			if($_SESSION['staff_sid']==session_id()){
+				header("location:404.php");		
+			}else{
+				header("location:login.php");
+			}
+		}
+	}
+?>
