efb976cbf0df040f755a06eafb4ac1a229353339
Murasakinosora <joshuatolentino44444@gmail.com>
2022-07-25 02:46:50 +0800

    add and delete users

--- src/webadmin/adminpanel.php
+++ src/webadmin/adminpanel.php
@@ -18,14 +18,28 @@ include '../includes/connectdb.php';
       <div class="">
         <div class=" ml-20 mt-20 md:mt-20 md:ml-60 md:mr-4 w-full h-60">
           <form class="" action="index.html" method="post">
-            <label for="user" class="font-bold w-2">Username:</label>
-            <input type="text" name="user" placeholder="Username" class="bg-blue-200 md:bg-blue-300 border-b-2 border-black" >
+
+
+						<br><label for="fname" class="font-bold">First Name:</label>
+						<input type="text" name="fname" placeholder="First Name" class="ml-0.5 bg-blue-200 md:bg-blue-300 border-b-2 border-black"><br>
+
+						<br><label for="lname" class="font-bold">Last Name:</label>
+						<input type="text" name="lname" placeholder="Last Name" class="ml-0.5 bg-blue-200 md:bg-blue-300 border-b-2 border-black"><br>
+
+            <br><label for="user" class="font-bold w-2">Username:</label>
+            <input type="text" name="user" placeholder="Username" class="bg-blue-200 md:bg-blue-300 border-b-2 border-black pl-1" ><br>
 
             <br><label for="pass" class="font-bold">Password:</label>
-            <input type="password" name="pass" placeholder="Password" class="ml-0.5 bg-blue-200 md:bg-blue-300 border-b-2 border-black"><br>
+            <input type="password" name="pass" placeholder="Password" class="ml-0.5 bg-blue-200 md:bg-blue-300 border-b-2 border-black pl-2"><br>
+						<br><label for="level" class="font-bold">User Level</label>
+						<select class="ml-0.5 bg-blue-200 md:bg-blue-300 border-b-2 border-black" name="level">
+							<option value="0">0</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+						</select>
 
-            <br><button type="submit" name="button" class="mt-5 border-black border-2 p-2 rounded-md">Add User</button>
-            <button type="submit" name="button" class="mt-5 border-black border-2 p-2 rounded-md">Delete User</button>
+            <br><button type="submit" name="button1" formaction="useradd.php" class="mt-5 border-black border-2 p-2 rounded-md">Add User</button>
+            <button type="submit" name="button2" formaction="userdelete.php" class="mt-5 border-black border-2 p-2 rounded-md">Delete User</button>
           </form>
         </div>
       </div>
