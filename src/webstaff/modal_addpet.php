0f1b689aa21378f817509fe4ae880fa92b793857
kibalias <74294078+kibalias@users.noreply.github.com>
2022-07-26 04:37:58 +0800

    Update for Pet List

--- src/webstaff/modal_addpet.php
+++ src/webstaff/modal_addpet.php
@@ -46,8 +46,8 @@
                               echo"<option value=$row[userID]>$row[user_firstname] $row[user_lastname]</option>";
                             }
                           ?>
-
                         </select>
+                        
                     </div>
                     <button type="submit" formaction="../crud/petlist_add.php" class="w-full text-black hover:text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add new service</button>
                 </form>
